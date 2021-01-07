01c5c9367f3c2c66cb0a797bd89e312338125c38
Nuri Benbarka <nuribenbarka@gmail.com>
2021-01-07 11:48:13 +0100

    Update siren_pytorch.py
    Hi, 
    From what I understood from your paper, only W0_initial is 30, and for the rest of the layers, W0 should be 1.

--- siren_pytorch/siren_pytorch.py
+++ siren_pytorch/siren_pytorch.py
@@ -45,7 +45,7 @@ class Siren(nn.Module):
 # siren network
 
 class SirenNet(nn.Module):
-    def __init__(self, dim_in, dim_hidden, dim_out, num_layers, w0 = 30., w0_initial = 30., use_bias = True, final_activation = None):
+    def __init__(self, dim_in, dim_hidden, dim_out, num_layers, w0 = 1., w0_initial = 30., use_bias = True, final_activation = None):
         super().__init__()
         layers = []
         for ind in range(num_layers):

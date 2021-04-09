e967362ff2e88cdde77fcc373d88262760351f8c
Phil Wang <lucidrains@gmail.com>
2021-04-09 11:58:41 -0700

    add ability to modulate Siren, for improved quality, based on new Adobe paper

--- README.md
+++ README.md
@@ -90,15 +90,64 @@ loss.backward()
 pred_img = wrapper() # (1, 3, 256, 256)
 ```
 
+## Modulation with Latent Code
+
+A <a href="https://arxiv.org/abs/2104.03960">new paper</a> proposes that the best way to condition a Siren with a latent code is to pass the latent vector through a modulator feedforward network, where each layer's hidden state is elementwise multiplied with the corresponding layer of the Siren.
+
+You can use this simply by setting an extra keyword `latent_dim`, on the `SirenWrapper`
+
+```python
+import torch
+from torch import nn
+from siren_pytorch import SirenNet, SirenWrapper
+
+net = SirenNet(
+    dim_in = 2,                        # input dimension, ex. 2d coor
+    dim_hidden = 256,                  # hidden dimension
+    dim_out = 3,                       # output dimension, ex. rgb value
+    num_layers = 5,                    # number of layers
+    w0_initial = 30.                   # different signals may require different omega_0 in the first layer - this is a hyperparameter
+)
+
+wrapper = SirenWrapper(
+    net,
+    latent_dim = 512,
+    image_width = 256,
+    image_height = 256
+)
+
+latent = torch.randn(512)
+img = torch.randn(1, 3, 256, 256)
+
+loss = wrapper(img, latent = latent)
+loss.backward()
+
+# after much training ...
+# simply invoke the wrapper without passing in anything
+
+pred_img = wrapper(latent = latent) # (1, 3, 256, 256)
+```
+
 ## Citations
 
 ```bibtex
 @misc{sitzmann2020implicit,
-    title={Implicit Neural Representations with Periodic Activation Functions},
-    author={Vincent Sitzmann and Julien N. P. Martel and Alexander W. Bergman and David B. Lindell and Gordon Wetzstein},
-    year={2020},
-    eprint={2006.09661},
-    archivePrefix={arXiv},
-    primaryClass={cs.CV}
+    title   = {Implicit Neural Representations with Periodic Activation Functions},
+    author  = {Vincent Sitzmann and Julien N. P. Martel and Alexander W. Bergman and David B. Lindell and Gordon Wetzstein},
+    year    = {2020},
+    eprint  = {2006.09661},
+    archivePrefix = {arXiv},
+    primaryClass = {cs.CV}
+}
+```
+
+```bibtex
+@misc{mehta2021modulated,
+    title   = {Modulated Periodic Activations for Generalizable Local Functional Representations}, 
+    author  = {Ishit Mehta and Michaël Gharbi and Connelly Barnes and Eli Shechtman and Ravi Ramamoorthi and Manmohan Chandraker},
+    year    = {2021},
+    eprint  = {2104.03960},
+    archivePrefix = {arXiv},
+    primaryClass = {cs.CV}
 }
 ```
--- siren_pytorch/siren_pytorch.py
+++ siren_pytorch/siren_pytorch.py
@@ -9,6 +9,9 @@ from einops import rearrange
 def exists(val):
     return val is not None
 
+def cast_tuple(val, repeat = 1):
+    return val if isinstance(val, tuple) else ((val,) * repeat)
+
 # sin activation
 
 class Sine(nn.Module):
@@ -53,13 +56,16 @@ class Siren(nn.Module):
 class SirenNet(nn.Module):
     def __init__(self, dim_in, dim_hidden, dim_out, num_layers, w0 = 1., w0_initial = 30., use_bias = True, final_activation = None):
         super().__init__()
-        layers = []
+        self.num_layers = num_layers
+        self.dim_hidden = dim_hidden
+
+        self.layers = nn.ModuleList([])
         for ind in range(num_layers):
             is_first = ind == 0
             layer_w0 = w0_initial if is_first else w0
             layer_dim_in = dim_in if is_first else dim_hidden
 
-            layers.append(Siren(
+            self.layers.append(Siren(
                 dim_in = layer_dim_in,
                 dim_out = dim_hidden,
                 w0 = layer_w0,
@@ -67,19 +73,49 @@ class SirenNet(nn.Module):
                 is_first = is_first
             ))
 
-        self.net = nn.Sequential(*layers)
-
         final_activation = nn.Identity() if not exists(final_activation) else final_activation
         self.last_layer = Siren(dim_in = dim_hidden, dim_out = dim_out, w0 = w0, use_bias = use_bias, activation = final_activation)
 
-    def forward(self, x):
-        x = self.net(x)
+    def forward(self, x, mods = None):
+        mods = cast_tuple(mods, self.num_layers)
+
+        for layer, mod in zip(self.layers, mods):
+            x = layer(x)
+
+            if exists(mod):
+                x *= rearrange(mod, 'd -> () d')
+
         return self.last_layer(x)
+
+# modulatory feed forward
+
+class Modulator(nn.Module):
+    def __init__(self, dim_in, dim_hidden, num_layers):
+        super().__init__()
+        dim = dim_in
+        self.layers = nn.ModuleList([])
+
+        for ind in range(num_layers):
+            is_first = ind == 0
+            dim = dim_in if is_first else dim_hidden
+
+            self.layers.append(nn.Sequential(
+                nn.Linear(dim, dim_hidden)
+            ))
+
+    def forward(self, x):
+        hiddens = []
+
+        for layer in self.layers:
+            x = layer(x)
+            hiddens.append(x)
+
+        return tuple(hiddens)
 
 # wrapper
 
 class SirenWrapper(nn.Module):
-    def __init__(self, net, image_width, image_height):
+    def __init__(self, net, image_width, image_height, latent_dim = None):
         super().__init__()
         assert isinstance(net, SirenNet), 'SirenWrapper must receive a Siren network'
 
@@ -87,16 +123,30 @@ class SirenWrapper(nn.Module):
         self.image_width = image_width
         self.image_height = image_height
 
+        self.modulator = None
+        if exists(latent_dim):
+            self.modulator = Modulator(
+                dim_in = latent_dim,
+                dim_hidden = net.dim_hidden,
+                num_layers = net.num_layers
+            )
+
         tensors = [torch.linspace(-1, 1, steps = image_width), torch.linspace(-1, 1, steps = image_height)]
         mgrid = torch.stack(torch.meshgrid(*tensors), dim=-1)
         mgrid = rearrange(mgrid, 'h w c -> (h w) c')
         self.register_buffer('grid', mgrid)
 
-    def forward(self, img = None):
+    def forward(self, img = None, *, latent = None):
+        modulate = exists(self.modulator)
+        assert not (modulate ^ exists(latent)), 'latent vector must be only supplied if `latent_dim` was passed in on instantiation'
+
+        mods = self.modulator(latent) if modulate else None
+
         coords = self.grid.clone().detach().requires_grad_()
-        out = self.net(coords)
+        out = self.net(coords, mods)
         out = rearrange(out, '(h w) c -> () c h w', h = self.image_height, w = self.image_width)
 
         if exists(img):
             return F.mse_loss(img, out)
+
         return out
